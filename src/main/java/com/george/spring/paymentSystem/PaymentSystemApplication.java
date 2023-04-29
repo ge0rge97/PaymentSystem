a30f36dd71982378dc0f8112d9620f2dc36a7f52
George <george.groshev1997@gmail.com>
2023-04-29 11:27:56 +0300

    0.1

---
+++ src/main/java/com/george/spring/paymentSystem/PaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.george.spring.paymentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/george/spring/paymentSystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.spring.paymentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
